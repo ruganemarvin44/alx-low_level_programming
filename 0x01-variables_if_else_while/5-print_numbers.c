b612c58fed96e2b538cb5cd805d72ec434db67f7
Marvin <ruganemarvin44@gmail.com>
2022-12-13 04:45:30 -0800

    Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function prints gifits of base  ten
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d \n", i);
+	return (0);
+}
