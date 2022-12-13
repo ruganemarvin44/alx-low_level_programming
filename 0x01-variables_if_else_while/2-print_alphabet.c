95a261dd071eff2d9556db308c6c0972d71d9ca8
Marvin <ruganemarvin44@gmail.com>
2022-12-13 02:03:34 -0800

    A to Z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdout.h>
+/**
+ * main - main function prints alphabetic in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char i = {a..z}
+	{
+		putchar(i);
+	}
+
+	return (0);
+}
