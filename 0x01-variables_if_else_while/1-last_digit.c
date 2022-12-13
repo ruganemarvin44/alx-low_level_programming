ee437d866157436a4f986155fb65d0e107c706b7
Marvin <ruganemarvin44@gmail.com>
2022-12-13 01:17:23 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,13 +22,13 @@ int main(void)
 	{
 		printf("%d and is greater than 5 \n", num);
 	}
-	else if (num == 0)
-	{
-		printf("%d and is 0 \n", num);
-	}
 	else if ((num < 6) && (num != 0))
 	{
 		printf("%d and is less than 6 and not 0 \n", num);
+	}
+	else
+	{
+		printf("%d and is 0\n", num);
 	}
 	return (0);
 }
