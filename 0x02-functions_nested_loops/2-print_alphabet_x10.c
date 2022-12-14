d431304c83483f7783f36d1175fcc65a66cedc86
Marvin <ruganemarvin44@gmail.com>
2022-12-14 01:36:11 -0800

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char c = 'a';
+
+	while (count++ <= 9)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+	}
+	_putchar('\n');
+
