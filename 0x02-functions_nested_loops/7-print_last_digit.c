fa36d917492c2498dfbaebb0191e38a3c76640bb
Marvin <ruganemarvin44@gmail.com>
2022-12-14 06:52:31 -0800

    LD

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,12 @@
  */
 int print_last_digit(int n)
 {
-	int x = n % 10;
+	int ld = n % 10;
 
-	if (x < 0)
-		x *= -1;
+	if (ld < 0)
+		ld *= -1;
 
-	_putchar(x + '0');
+	_putchar(ld + '0');
 
 	return (0);
 }
