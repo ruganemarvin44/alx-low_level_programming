b896e31195a5e15d5f8860b32b3aa6959752db3c
Marvin <ruganemarvin44@gmail.com>
2022-12-14 07:14:42 -0800

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult - 1; mult <= 9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -23,7 +23,7 @@ void times_table(void)
 			else
 				_putchar((prod / 10) + '0');
 
-			_putchar((prod % 10 + '0');
+			_putchar((prod % 10) + '0');
 		}
 	}
 }
