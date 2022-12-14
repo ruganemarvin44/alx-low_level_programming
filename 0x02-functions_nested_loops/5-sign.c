e578a0c650b6cfa1593f982125e3abc519202476
Marvin <ruganemarvin44@gmail.com>
2022-12-14 04:34:08 -0800

    Greater than 1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,9 @@
 /**
  * print_sign - prints sign of a number
  * @n: character that is measured against
- * Return: 1 if n is greater than 0 and 0 otherwise.
+ * Returns 1 and prints + if n is greater than zero
+ * 0 and orints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -16,7 +18,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
