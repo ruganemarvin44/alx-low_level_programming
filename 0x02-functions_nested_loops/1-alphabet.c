5a8304408117332be502abcabf52968683a4f773
Marvin <ruganemarvin44@gmail.com>
2022-12-14 01:05:51 -0800

    A to Z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	(ch = 'a');
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar('ch');
+		_putchar('\n');
+
+	}
+	return (0);
+}
