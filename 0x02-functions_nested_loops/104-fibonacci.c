0e41526a131144928e879c6a088f1d486e849fb5
Marvin <ruganemarvin44@gmail.com>
2022-12-15 01:32:02 -0800

    FIB

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 	int count;
-	unsigned long fib = 0, fib2 = 1, sum;
-	unsigned long fib_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
@@ -27,7 +27,7 @@ int main(void)
 	{
 	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
-	if (fib1_half2 + fib_half2 > 9999999999)
+	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
 	half1 += 1;
 	half2 %= 10000000000;
