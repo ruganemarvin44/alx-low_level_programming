a0b8fb4bbb3465cdcfb6a9186c2dd27dcabc62a0
Marvin <ruganemarvin44@gmail.com>
2022-12-16 04:20:52 -0800

    Digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,17 +2,15 @@
 /**
  * _isdigit - Checks for a digit 0 through 9
  *@c: chaaracter to be checked
+
  * Return: 1 if c is a digit and,
  * 0 otherwise
  */
 int _isdigit(int c)
 {
 	if (c <= 0 || c >= 9)
-	{
-	return (1);
-	}
+		return (1);
 	else
-	{
-	return (0);
-	}
+		return (0);
+	
 }
