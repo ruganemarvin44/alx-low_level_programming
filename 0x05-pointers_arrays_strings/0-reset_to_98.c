c4baf1d678fa883186c379d02bd8b3d97a1d0d04
Marvin <ruganemarvin44@gmail.com>
2022-12-19 06:23:12 -0800

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reset_to_98 - Pointer points to 98
+ * @n: character to be reset
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	int i
+	int *n
+
+	i = 402;
+	n = &i;
+	*n = 98;
+	printf("%d\n", i);
+	printf("%d\n", &n);
+	return (0);
+}
