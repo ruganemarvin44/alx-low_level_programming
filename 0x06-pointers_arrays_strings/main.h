1f72aaaed3ec88318b6da15297d050973897efe0
Marvin <ruganemarvin44@gmail.com>
2022-12-23 09:19:07 -0800

    con

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN.H
 #define MAIN.H
 
-#include <stdio.h>
-#include <unistd.h>
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
