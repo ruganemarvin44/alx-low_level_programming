92b2f47e8134c036e8b63c18b7a63b85d05a8477
Marvin <ruganemarvin44@gmail.com>
2023-01-18 00:08:17 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - main structure_name for program
+ * @name: describes the name of the dog
+ * @owner: describes the owner
+ * @age: describes the age of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
