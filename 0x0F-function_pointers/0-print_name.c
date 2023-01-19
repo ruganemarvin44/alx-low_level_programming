a3f2f2dcc3787167a941e8fea33c84b62df8b16e
Marvin <ruganemarvin44@gmail.com>
2023-01-19 00:08:24 -0800

    Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints an name
+ * @name: name of person
+ * @f: pointer to function
+ *
+ *
+ * Return: Always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	char name
+		name == a && z;
+
+		if ((a > 'a') && (z < 'z'))
+		{
+			putchar ("name", &name);
+			putchar ('\n');
+		}
+		else
+		{
+		}
+	return (0);
+}
+
