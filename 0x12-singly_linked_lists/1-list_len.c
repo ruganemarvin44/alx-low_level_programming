16bec0d5d439eab2db4f7b6b2ab7eb2f14060dde
Marvin <ruganemarvin44@gmail.com>
2023-01-30 11:47:20 -0800

    Number in List

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,4 +2,21 @@
 #include "lists.h"
 
 /**
- *
+ * list_len - calculate the number of elements
+ * @h: pointer to a list
+ * Return: integer
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	unsigned int counter = 0;
+
+	temp = h;
+	while (temp)
+	{
+		counter++;
+		temp = temp->next;
+	}
+	return (counter);
+}
+
