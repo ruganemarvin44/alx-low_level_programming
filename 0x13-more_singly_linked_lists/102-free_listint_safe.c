8acfbaa225fdd4ddba1e298439e9da5e67acb7d4
Marvin <ruganemarvin44@gmail.com>
2023-02-02 13:19:40 -0800

    print

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,34 +5,34 @@
  *
  * Return: number of elements in the freed list
  */
-size_t print_listint_safe(const listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+size_t len = 0;
+int diff;
+listint_t *temp;
 
-	if (!h || !*h)
-		return (0);
+if (!h || !*h)
+return (0);
 
-	while (*h)
-	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			free(*h);
-		 *h = NULL;
-		 len++;
-		 break;
-		}
-	}
-	*h = NULL;
+while (*h)
+{
+diff = *h - (*h)->next;
+if (diff > 0)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+len++;
+}
+else
+{
+free(*h);
+*h = NULL;
+len++;
+break;
+}
+}
+*h = NULL;
 
-	return (len);
+return (len);
 }
